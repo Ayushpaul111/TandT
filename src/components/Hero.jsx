c157e80207aa4b98abf5dfed81547c43b556ee16
Ayush Paul <ayushpaul1111@gmail.com>
2024-02-16 23:05:15 +0530

    testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            Bringing <br className="sm:block hidden" />{" "}
+            Bringing<br className="sm:block hidden" />{" "}
             <span className="text-gradient">Some Magic</span>{" "}
           </h1>
           {/* <div className="ss:flex hidden md:mr-4 mr-0">
